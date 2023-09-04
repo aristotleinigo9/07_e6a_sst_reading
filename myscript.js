a43a2dee83016f9296d5bf9b80aece2e9881d0a0
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 13:11:02 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,339 @@
+var myQuestions = [
+  
+  {
+    question: "<p style = 'color:green'> Passage 1 </p> <p style ='text-align: center'> AT THE CLINIC </p> <p style = 'text-align:justify'>Luis had a headache, a sore throat, a bad cough, and a temperature. He had been sick for five days. Finally, his wife, Martha persuaded him to go to the Furnell University health Clinic. He didn’t have an appointment, so he knew he would have to wait. <p style = 'text-align:justify'> As he entered the clinic, the receptionist asked him to fill out a sign-in slip. He wrote down his name, address, phone number, and I.D. number. Then he checked the reason for his visit to the clinic. He checked 'cold, sore throat'.</p><p style = 'text-align:justify'>Luis waited in the waiting room on the second floor for about twenty minutes. Then, a nurse called his name and Luis followed her into an examination room. She took his temperature and told him the health associate would be in to see him soon. Luis asked the nurse if a health associate was a doctor. She explained that a health associate is not a doctor but has several years of medical education and training. When necessary, health associates consult with the clinic doctors. <p style = 'text-align:justify'>After fifteen minutes the health associate came in. Luis described his symptoms to her. After she had examined him, she told him that his throat was very red, his neck glands were quite swollen, and his temperature was 102 degrees “You’ve got a bad infection, Luis. I’m going to ask the doctor to prescribe an antibiotic for you. Take one capsule three times a day for ten days. You should also stay in bed for two days until your fever comes down. Drink a lot of liquid and rest as much as possible. If your infection doesn’t clear up after five days, come back to the clinic or call me.”<p style = 'text-align:justify'> “Thanks a lot,” said Luis, taking the prescription. He was glad that he had listened to his wife’s advice. On his way home, he stopped at the drug store and had his prescription filled.<p style = 'text-align:justify'> When he got home, he took his medicine and went right to bed. Two days later his temperature was back to normal. By the time he had taken all the pills, he felt like himself again. </p> 1.	Health associates _________________."
+    ,
+    answers: {
+      a: "are better trained than doctors",
+      b: 'are not as intelligent as doctors',
+      c: 'prefer working with students at a university',
+      d: 'work closely with doctors at the clinic'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "2. A receptionist's job is to _______________.",
+    answers: {
+      a: 'prescribe medicine',
+      b: "talk to people as they enter",
+      c: 'cleans offices',
+      d: "take people's temperature'"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "3. One of the following was not a  symptom of Luis’ infection.",
+    answers: {
+      a: 'stomach ache',
+      b: 'sore throat',
+      c: 'fever',
+      d: 'headache'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "4. The health associate told him to _______.",
+    answers: {
+      a: 'drink a lot of liquid.',
+      b: 'do back to class immediately.',
+      c: 'dake two capsules every four hours.',
+      d: 'eat a low-fat diet.'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "5.	One of the following statements is true.",
+    answers: {
+      a: "Luis didn’t have to wait for medical attention.",
+      b: 'The clinic gave Luis the necessary medicine.',
+      c: 'Luis’ fever did not come down after he took the pills.',
+      d: 'His fever came down in two days.'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "6. A health associate receives his education in __________________.",
+    answers: {
+      a: 'ITCs',
+      b: 'nursery schools',
+      c: 'medical schools',
+      d: 'commercial schools'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "7. One of the followings will help to clear up an infection.",
+    answers: {
+      a: 'taking antibiotics',
+      b: 'exercising',
+      c: 'examining the doctor',
+      d: 'eating a lot'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "<p style = 'color:green'> Passage 2 </p> <p style ='text-align: center'> THE HIGH COST OF HEALTH CARE IN THE UNITED STATES</p> <p style = 'text-align:justify'> The cost of health care in the United States is very high. The time and money that doctors spend on their medical education is probably one reason for this problem.<p style = 'text-align:justify'> A visit to a doctor’s office costs from fifteen to fifty dollars. It is almost impossible for people to pay for the medical care they need. Many people in the United States think that doctors are overpaid. Most doctors, however, disagree. They say that they were required to study medicine for a very long time, first as undergraduates, then as graduate students. Tuition for many years of medical education costs a lot of money. Doctors argue that it is necessary for most medical students to borrow money from a bank to pay their tuition. Because this money must be repaid to the bank, young doctors need to receive a lot of money for their work. So, they charge people high prices for medical care.Therefore, it is possible that the high cost of medical care in America is unnecessary. Because high tuition is one cause of high costs, one way to lower costs would be to have medical schools that are free or have low tuition fees.<p style = 'text-align:justify'>8. It is impossible for some people to pay for their medical care.  These people _________. ",
+    answers: {
+      a: 'do not want to pay for medical care',
+      b: 'want to pay for medical care',
+      c: 'cannot pay for medical care',
+      d: 'can pay for medical care soon'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "9. Many people say that doctors are overpaid.  They say that doctors _____________.",
+    answers: {
+      a: 'are paid too much',
+      b: 'are paid too little',
+      c: 'are paid too early',
+      d: 'are paid too late'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "10. When people say that doctors are overpaid, most doctors disagree.  The doctors _______.",
+    answers: {
+      a: 'say the same thing',
+      b: 'return the money',
+      c: 'do not agree',
+      d: 'go to medical school'
+
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "11. Medical students often borrow money from a bank. Later, when they are doctors, It must be repaid.  They must __________.",
+    answers: {
+      a: 'charge people lower prices',
+      b: 'go to the bank more often',
+      c: 'pay by check or credit card',
+      d: 'give the money back to the bank'
+
+    },
+    correctAnswer: 'd'
+  },
+  
+  {
+    question: "12. Maybe the high cost of medical care is unnecessary. Maybe it is __________.",
+    answers: {
+      a: 'very necessary',
+      b: 'not needed',
+      c: 'very important',
+      d: 'the price you have to pay'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "13. Medical care is very expensive in the United States because _____________.",
+    answers: {
+      a: 'medical schools charge a lot of money',
+      b: 'medical students are overpaid',
+      c: 'medical education tuition costs very littel',
+      d: 'medical students argue a lot'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "14. In America, doctors may ask you to pay _______________.",
+    answers: {
+      a: 'less than fifteen dollars',
+      b: 'between fifteen and fifty dollars',
+      c: 'more than fifty dollars',
+      d: 'whatever you can afford'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "<p style = 'color:green'> Passage 3 </p> <p style = 'text-align:justify'> Thirty-six percent of freshmen or first-year students who enter a large New York university drop out of the university after the first year. A study showed that this shocking rate of failure is due to a variety of causes some of which are out of the control of the student. Among these are insufficient advising on school subjects and personal problems that the first year student may face. Other causes are the poor quality of the teaching in very large freshmen classes and the computerized way that the student is handled. However, the biggest cause of early student failure goes back to the student’s inability to adapt to the university.</p> 15. This passage is about _______________.",
+    answers: {
+      a: 'the number of students in a large New York university',
+      b: 'how a large New York university works',
+      c: 'why first-year students fail at a certain university',
+      d: 'the quality of teaching at a certain New York university'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "16. The passage mentions __________ reasons why students are dropped from the university after the first year.",
+    answers: {
+      a: 'four',
+      b: 'five',
+      c: 'six',
+      d: 'seven'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "17. The most important reason for student failure is ______________.",
+    answers: {
+      a: 'very large freshmen classes',
+      b: 'the computerized way the student is handled',
+      c: 'insufficient help in course work',
+      d: 'the inability of the student to adapt'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<p style = 'color:green'> Passage 4 </p> <p style = 'text-align:justify'>During the past five years, the number of Americans killed in car accidents yearly has climbed to more than 55,000. This needless killing on streets and highways can be attributed to three general causes. Mechanical failures, especially those related to faulty brakes and bald tires, account for a large number of fatal accidents. Environmental conditions, such as blind corners, narrow streets, fog, and snow which causes slippery road surfaces also contribute to the horrible accident statistics. However, without question, the most frequently reported factor in car accidents are mistakes in human judgment. These mistakes include things like excessive speeding, failure to signal a turn or a change from one lane to another, and lack of concentration on the part of the driver. The man behind the wheel is often his own worst enemy.</p>18. The three common causes of car accidents, according to the writer of this paragraph, are _______________.",
+    answers: {
+      a: 'mechanical failures, environmental conditions and human errors',
+      b: 'blind corners, fog and snow',
+      c: 'excessive speed, lack of concentration, and failure to signal a turn',
+      d: 'faulty brakes, speeding and bad driving'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "19. An example of an environmental factor would be ______________.",
+    answers: {
+      a: 'rain',
+      b: 'a human mistake',
+      c: 'a faulty signal',
+      d: 'poor concentration'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "20. The passage is mainly about ______________.",
+    answers: {
+      a: "the environmental factors that cause fatal accidents",
+      b: "the number of Americans killed in car accidents every year",
+      c: "the needless killing of humans on the streets and highways of America",
+      d: 'the main causes of fatal car accidents in America '
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "<p style = 'color:green'> Passage 5 </p> <p style = 'text-align:justify'>In America, everyone is given a Social Security number. Each person’s Social Security number consists of nine numbers written in a form like this: 308-50-6037. There was a Social Security number, 078-05-1120, that was used a lot before 1977. This was the number on a sample Social Security card that a wallet manufacturer had used to put in each wallet he manufactured in order to show how cards of that size would fit into his wallets. By the way, that number was his secretary’s number. Although the card was marked “SAMPLE”, many buyers of these wallets believed that it was the number assigned to them by the government. Therefore, tens of thousands of Social Security deductions on tax forms were credited to account 078-05-1120. It took 20 years to straighten out the problem. As for the secretary, she was given a new number. Otherwise, among the payments to her account would have been millions of dollars in retirement benefits.</p>21. This passage is mainly about ______________.",
+    answers: {
+      a: "how everyone in America is given a Social Security number",
+      b: "the importance of the Social Security number 078-05-1120",
+      c: "how Social Security number 078-05-1120 caused problems",
+      d: 'how a secretary received money from the government'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "22. The Social Security number 078-05-1120 was used ______________.",
+    answers: {
+      a: "as a sample put into a kind of wallet",
+      b: "from 1977 until recently",
+      c: "to straighten out a tax problem",
+      d: "to give a secretary some retirement benefits"
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "23. To solve the problem with the number, the secretary ______________.",
+    answers: {
+      a: "was asked to return the millions of dollars she received",
+      b: "changed her account number at her bank",
+      c: "was given a new Social Security number",
+      d: "complained to the government about people using her number"
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "24. It can be understood from the passage that _______________.",
+    answers: {
+      a: "some people today may have the same Social Security number",
+      b: "every person in America has a different Social Security number",
+      c: "only people that pay taxes to the government have Social Security numbers",
+      d: "you can get rich in America if you use other people’s Social Security numbers"
+    },
+    correctAnswer: 'b'
+  },
+  
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
